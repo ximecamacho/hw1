f9ff8c561b3aeb21eb41b6537cfc22ed8ad8cf3d
Ximena Camacho <xcamacho@usc.edu>
2025-02-02 00:51:13 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -13,11 +13,78 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+Node* findNextOdd(Node*& in);
+Node* findNextEven(Node*& in);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
+/* Add code here */
 // WRITE YOUR CODE HERE
+
+//if at end of in/ in is empty, finish
+if(in==nullptr){
+  return;
+}
+
+if(((in->value)%2)!=0){
+  //odd case
+  Node* tempOdd=in->next;
+  //initializes odd node with current value and looks
+  //for next odd value for next odd list item
+  odds=in;
+  odds->next=findNextOdd(in);
+  split(tempOdd, odds->next, evens);
+
+}
+else{
+  //even case
+  Node* tempEven=in->next;
+  //initializes even node with current value and looks
+  //for next even value for next even list item
+  evens=in;
+  evens->next=findNextEven(in);
+  split(tempEven, odds, evens->next);
+}
+
+//sets in to null
+in=nullptr;
+
 }
 
 /* If you needed a helper function, write it here */
+
+//finds next odd node
+Node* findNextOdd(Node*& in){
+  //if next value is null, return
+  if(in->next==nullptr){
+     return nullptr;
+  }
+  //if next value is not odd, keep searching
+  if((in->next->value%2)==0){
+    findNextOdd(in->next);
+  }
+  else{
+    //if next value is odd, return it
+    return (in->next);
+  }
+  return nullptr;
+
+}
+
+//finds next even node
+Node* findNextEven(Node*& in){
+  //if next value is null, return
+  if(in->next==nullptr){
+    return nullptr;
+  }
+  //if next value is not even, keep searching
+   if((in->next->value%2)!=0){
+    findNextEven(in->next);
+  }
+  else{
+    //if next value is even, return it
+    return (in->next);
+  }
+  return nullptr;
+
+}
--- test_split.cpp
+++ test_split.cpp
@@ -13,5 +13,31 @@ g++ split.cpp test_split.cpp -o test_split
 
 int main(int argc, char* argv[])
 {
+  
+  Node* head=new Node(1,nullptr);
+  Node* currTail=head;
+  for(int i=2; i<10; i++){
+    Node* newNode= new Node(i,nullptr);
+    currTail->next=newNode;
+    currTail=newNode;
+  }
+  Node* odds=nullptr;
+  Node* evens=nullptr;
+
+  split(head,odds,evens);
+
+  Node* currElement=odds;
+  while (currElement!=nullptr){
+    Node*nextElement=currElement->next;
+    delete currElement;
+    currElement= nextElement;
+  }
+
+  Node* currElement2=evens;
+  while (currElement2!=nullptr){
+    Node*nextElement2=currElement2->next;
+    delete currElement2;
+    currElement2= nextElement2;
+  }
 
 }
